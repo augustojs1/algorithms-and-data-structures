54463303c2543c1e1e494321768fd8fd0f90a70d
augustojs1 <augustojsouza1@gmail.com>
2024-01-08 20:00:32 -0300

    feat: created function that implements bubble sort algorithm

--- chapter-4/bubble_sort.js
+++ chapter-4/bubble_sort.js
@@ -1,7 +1,25 @@
 const array = [34, 90, 23, 50, 78];
 
 function bubbleSort(array) {
-  for (let i = 0; i <= array.length; i++) {}
+  let unsortedUntilIndex = array.length - 1;
+  let sorted = false;
+
+  while (!sorted) {
+    sorted = true;
+
+    for (let i = 0; i <= unsortedUntilIndex; i++) {
+      if (array[i] > array[i + 1]) {
+        let currentLeftSideValue = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = currentLeftSideValue;
+        sorted = false;
+      }
+    }
+
+    unsortedUntilIndex -= 1;
+  }
+
+  return array;
 }
 
 console.log(bubbleSort(array));
